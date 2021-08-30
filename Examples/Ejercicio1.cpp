cf3f32046510182ac49f59df51499fae52d9dd9a
jcontrolresearch <jcmr.ee.91gmail.com>
2021-08-30 12:21:05 -0500

    Added Ejercicio1 to examples

---
+++ Examples/Ejercicio1.cpp
@@ -0,0 +1,37 @@
+// "//" Se usa para comentarios de una linea
+// Que es un comentario-> un comentarios es texto que exciste en codigo pero que no es 
+// interpretado por el compilador
+
+/* Los simbolos '/*' abren un bloque de comentarios
+
+Estos e sun bloque de comentarios
+
+Los simbolos de cerrar bloque de comentarios son: 
+*/ 
+
+//************************************************************
+
+/*
+Hacer un algoritmo que imprima sus datos personales(nombre, apellido, edad, matricula universitaria)
+Autor: Julio Martinez
+Fecha: 18/08/2021
+*/
+
+// Tenemos 0 entradas y 4 salidas
+
+//Inicio
+#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//#include <string.h>
+//stdio.h hace referencia a std- standard / io - input ouput
+// biblioteca genereica para entradas y salidas
+
+// Debe haber una funcion en la biblioteca que acabamos de agregar que imprima texto
+// La funcion printf imprime el valor en pantalla
+int main(){
+	printf("Julio");
+	return 0;
+}
+
+//fin
